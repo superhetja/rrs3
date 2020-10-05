6991018cc75f2ab7b54a2e6a2331308768104c81
holmfridur <holmfridurh17@ru.is>
2020-10-05 23:57:02 +0000

    KD tree komið vel á leið

--- KdTree.java
+++ KdTree.java
@@ -26,6 +26,7 @@ public class KdTree {
         private RectHV rect; // the axis-aligned rectangle corresponding to this node
         private Node lb; // the left/bottom subtree
         private Node rt; // the right/top subtree
+        //private String lvl;
     }
 
     // is the set empty?
@@ -40,12 +41,60 @@ public class KdTree {
 
     // add the point p to the set (if it is not already in the set)
     public void insert(Point2D p) {
-            root = insert(p, root);
+            root = insert(p, root, X);
 
     };
 
-    private Node insert(Point2D p, Node n){
+    private Node insert(Point2D p, Node n,String lvl){
+        if (n == null){
+            RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
+            return createNode(p, rect);
+        }
+        if (lvl == X){
+            if (p.x() < n.p.x()) {
+                if(n.lb != null) {n.lb = insert(p, n.lb, Y);}
+                else {
+                    RectHV rect = new RectHV(n.rect.xmin(), n.rect.ymin(), n.rect.xmax(), p.y());
+                    n.lb = createNode(p, rect);
+                    return n;
+                }
+            }
+            else { // if(p.x() >= n.p.x())
+                if (n.rt != null) { n.rt = insert(p, n.rt, Y); }
+                else {
+                    RectHV rect = new RectHV(n.rect.xmin(), p.y(), n.rect.xmax(), n.rect.ymax());
+                    n.rt = createNode(p, rect);
+                    return n;
+                }
+                }
+        }
+        else {  //if (lvl == Y)
+            if (p.y() < n.p.y()) {
+                if (n.lb != null ) { n.lb = insert(p, n.lb, X); }
+                else {
+                    RectHV rect = new RectHV(n.rect.xmin(), n.rect.ymin(), p.x(), n.rect.ymax());
+                    n.lb = createNode(p, rect);
+                    return n;
+                }
+            }
+            else { // if(p.y() >= n.p.y())
+                if ( n.rt != null ) { n.rt = insert(p, n.rt, X); }
+                else {
+                    RectHV rect = new RectHV(p.x(), n.rect.ymin(), n.rect.xmax(), n.rect.ymax());
+                    n.rt = createNode(p, rect);
+                    return n;
+                }
+            }
+        }
+        return n; //if removed, java complains about missing return statement
+    }
 
+    private Node createNode (Point2D p, RectHV r) {
+        Node newNode = new Node();
+        newNode.p = p;
+        newNode.rect = r;
+        size++;
+        return newNode;
     }
 
     // does the set contain the point p?
@@ -56,20 +105,59 @@ public class KdTree {
 
     // draw all of the points to standard draw
     public void draw() {
-        for(Point2D point : tree.keys()){
-            point.draw();
 
-        }
     }
 
     // all points in the set that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        Queue<Point2D> inside = new Queue<>();
+        return range(rect, inside, root);
+    }
+
+    private Queue<Point2D> range(RectHV r, Queue<Point2D> inside, Node n)
+    {
+        if ( n==null ) { return inside; } // veit ekki með return inside
+        if (r.intersects(n.rect)) {
+            if (r.contains(n.p)){ inside.enqueue(n.p); }
+            inside = range(r, inside, n.lb);
+            inside = range(r, inside, n.rt);
+        }
+        return inside;
     }
 
     // a nearest neighbor in the set to p; null if set is empty
     public Point2D nearest(Point2D p) {
-        return p;
+        //Nearest neighbor search. To find a closest point to a given query point, start at the root and recursively
+        // search in both subtrees using the following pruning rule:
+        // if the closest point discovered so far is closer than the distance between the query point and the rectangle
+        // corresponding to a node, there is no need to explore that node (or its subtrees).
+        // That is, a node is searched only if it might contain a point that is closer than the best one found so far.
+        // The effectiveness of the pruning rule depends on quickly finding a nearby point. To do this, organize your
+        // recursive method so that when there are two possible subtrees to go down, you always choose the subtree that
+        // is on the same side of the splitting line as the query point as the first subtree to explore—the closest
+        // point found while exploring the first subtree may enable pruning of the second subtree.
+        return nearest(p, root, root.p);
+    }
+
+    private Point2D nearest(Point2D p, Node n, Point2D near){
+
+        if(n == null) { return near; }
+        if (near.distanceTo(p) < n.p.distanceTo(p) && near.distanceTo(p) < n.rect.distanceTo(p)){
+            return near;
+        }
+        if (near.distanceTo(p) > n.p.distanceTo(p)) { near = n.p; }
+        // TODO: virkar ekki þvi lb og rt getur verið null !!
+        if (n.lb.rect.contains(p)){
+            if (n.lb.rect.distanceTo(p) < near.distanceTo(p)) { near = nearest(p, n.lb, near); }
+            if (n.rt.rect.distanceTo(p) < near.distanceTo(p)) { near = nearest(p, n.rt, near); }
+        }
+
+        else { //(n.rt.rect.contains(p))
+            if (n.rt.rect.distanceTo(p) < near.distanceTo(p)) { near = nearest(p, n.rt, near); }
+            if (n.lb.rect.distanceTo(p) < near.distanceTo(p)) { near = nearest(p, n.lb, near); }
+        }
+
+        return near;
     }
 
     /*******************************************************************************
